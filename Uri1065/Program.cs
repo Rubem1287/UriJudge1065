74966b9062254b9be8133a1c8bf582299ffa6879
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:30:20 -0300

    Add files via upload

---
+++ Uri1065/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Uri1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f, cont;
+
+            
+            cont = 0;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            
+
+            if (a % 2==0)
+            {
+                
+                cont += +1;
+            }
+
+            if (b % 2 == 0)
+            {
+                ;
+                cont += +1;
+            }
+
+            if (c % 2 == 0)
+            {
+                
+                cont += +1;
+            }
+
+            if (d % 2 == 0)
+            {
+                
+                cont += +1;
+            }
+
+            if (e % 2 == 0)
+            {
+                
+                cont += +1;
+            }
+
+           
+            
+
+            Console.WriteLine();
+            Console.WriteLine(cont + " Valores pares");
+            
+        }
+    }
+}
